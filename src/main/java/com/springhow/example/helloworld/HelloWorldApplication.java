eb7804e387d7d78b7c59d4d4b6f481475ee00a0a
AbhayVerma007 <vermaabhay085@gmail.com>
2026-03-19 01:50:06 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping; // Modern practice uses GetMapping
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,15 +13,15 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // This method is critical for WAR deployment in Tomcat 10
         return application.sources(HelloWorldApplication.class);
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloWorldApplication.class);
+        SpringApplication.run(HelloWorldApplication.class, args);
     }
 
-
-    @RequestMapping("/")
+    @GetMapping("/")
     String helloWorld() {
         return "Welcome to my Online Learning Classes - Jenkins by Shikhar";
     }
